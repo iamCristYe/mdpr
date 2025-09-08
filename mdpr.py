f7d54ecb33986f03d8bc0e130dc1bad367c31c1a
user.name <user.email>
2025-09-08 20:54:47 +0800

    Test

--- mdpr.py
+++ mdpr.py
@@ -20,7 +20,7 @@ def parse_html():
         "User-Agent": "curl/8.5.0",
     }
     photos_html = requests.get(
-        "https://mdpr.jp/photo/detail/18770278", headers=headers
+        "https://mdpr.jp/photo/detail/18977428", headers=headers
     ).text
     with open("photos.html", "w", encoding="utf-8") as f:
         f.write(photos_html)
