5eec59ed47ff4bc7e0c069991dc3dc91a8b5de0d
Crist Ye <12642018+iamCristYe@users.noreply.github.com>
2025-09-08 19:32:51 +0800

    Update entameclip.py

--- entameclip.py
+++ entameclip.py
@@ -21,7 +21,7 @@ def parse_html():
         "User-Agent": "curl/8.5.0",
     }
     photos_html = requests.get(
-        "https://entameclip.com/picture/438138/", headers=headers
+        "https://entameclip.com/picture/453042/", headers=headers
     ).text
     with open("photos.html", "w", encoding="utf-8") as f:
         f.write(photos_html)
