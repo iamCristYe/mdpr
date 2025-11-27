f98ccfed3a51e76647dc3a7ea9975eecccfd109b
Crist Ye <12642018+iamCristYe@users.noreply.github.com>
2025-11-27 08:46:23 +0800

    Update mdpr.py

--- mdpr.py
+++ mdpr.py
@@ -20,7 +20,7 @@ def parse_html():
         "User-Agent": "curl/8.5.0",
     }
     photos_html = requests.get(
-        "https://mdpr.jp/photo/detail/18977428", headers=headers
+        "https://mdpr.jp/photo/detail/19354863", headers=headers
     ).text
     with open("photos.html", "w", encoding="utf-8") as f:
         f.write(photos_html)
